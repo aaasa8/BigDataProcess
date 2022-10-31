0a58bd281259a6707f1393169074cc1e90905590
aaasa8 <aaaaj123456@naver.com>
2022-10-31 23:34:14 +0900

    add HW3

---
+++ HW3/IMDBStudent20201002.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+file_input = sys.argv[1]
+file_output = sys.argv[2]
+
+f = open(file_input, "rt")
+for line in f:
+	movie_list = line.split("::")
+	genre = movie_list[2]
+f.close()
+
+genre_dic = dict()
+
+for g in genre:
+	genre_list = g.split("|")
+	for g2 in genre_list:
+		if g2 not in genre_dic:
+			genre_dic[g2] = 1
+		else:
+			genre_dic[g2] += 1
+f = open(file_output, "wt")
+for key, value in genre.items():
+	f.write(key, value)
+f.close()
